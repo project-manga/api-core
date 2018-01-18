d507e16a6cf279f03e41cf3a1104a8f03f40a2da
Andrea Bertin <andrea.bertin@outlook.com>
2018-01-18 07:03:24 +0100

    Added fake test for circleci
    Since I added run test in circle-ci config it tries to run tests. It seems not founding test breaks fail the process..

---
+++ api-core-app-test/CircleCiFakeTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ApiCoreAppTest
+{
+    [TestFixture]
+    public class CircleCiFakeTest
+    {
+        [Test]
+        public void Just_say_hi()
+        {
+            Assert.Inconclusive("Yet to add a meaning test, using this for circle ci integration");
+        }
+    }
+}
